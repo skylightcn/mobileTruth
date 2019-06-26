c08135401dcd7dc27d7ff303ec83f18d5d6b9010
v345 <343@gmail.com>
2019-06-26 22:12:41 +0800

    2

--- config/szzdData.js
+++ config/szzdData.js
@@ -1,68 +1,235 @@
-let dir=
-[{
-        name:'真相短视频：抑郁和不孕的她迎来了光明 ',
-        value:'Rb4n'
+let dir = [{
+        name: '过年',
+        value: 'wBZT'
+    },
+    {
+        name: '艺考',
+        value: 'TnkQ'
+    },
+    {
+        name: '小磊历险记',
+        value: 'q183'
+    }, {
+        name: '心愿',
+        value: 'uHpb'
+    },
+    {
+        name: '晓宇一家人',
+        value: 'm7FN'
+    },
+    {
+        name: '难忘的岁月',
+        value: 'mzvC'
+    },
+    {
+        name: '街对面的烛光',
+        value: '7CxZ'
+    },
+    {
+        name: '传奇时代',
+        value: 'pVRc'
+    },
+    {
+        name: '传奇时代 义诉',
+        value: 'video/play/1040.html'
+    },
+    {
+        name: '粤菜名厨的传奇人生,【传奇时代】',
+        value: '_ytb/KvkfL_-nA3k.mp4'
+    },
+    {
+        name: '雪中梅,【传奇时代】',
+        value: '_ytb/1DodyI7Gc1E.mp4'
+    },
+    {
+        name: '通往天安门的旅程,【传奇时代】',
+        value: '_ytb/b4BUg6H1Vqo.mp4'
+    },
+    {
+        name: '枪声过后,【传奇时代】',
+        value: '_ytb/jM4wRhgONCw.mp4'
+    },
+    {
+        name: '永恒的电波（上）,【传奇时代】',
+        value: '_ytb/YlqQQEd2t14.mp4'
+    },
+    {
+        name: '永恒的电波（下）,【传奇时代】',
+        value: '_ytb/iRKQh8Jjk78.mp4'
+    },
+    {
+        name: '功夫 尋道,【传奇时代】',
+        value: '_ytb/h6B4F_l0zqI.mp4'
+    },
+    {
+        name: '天國樂團 ,【传奇時代】',
+        value: '_ytb/e8WmZl85XYI.mp4'
+    },
+    {
+        name: '红墙的记忆1,【传奇時代】',
+        value: '_ytb/Th5JrblwFHE.mp4'
+    },
+    {
+        name: '红墙的记忆2,【传奇時代】',
+        value: '_ytb/eJmR6TlVrI4.mp4'
+    },
+    {
+        name: '终极目的',
+        value: 'npXG'
+    },
+
+    {
+        name: '希望之声直播',
+        value: 'RjrP'
+    },
+    {
+        name: '铁证如山',
+        value: 'video/play/1080.html'
+    },
+    {
+        name: '九评之一',
+        value: 'video/play/4.html'
+    },
+    {
+        name: '共产主义的终极目的',
+        value: 'video/play/1035.html'
+    },
+    {
+        name: '新唐人中国频道直播',
+        value: 'v.php?id=ntd'
+    },
+    {
+        name: '新唐人亚太台直播',
+        value: 'v.php?id=ntd-mlt'
+    },
+    {
+        name: '每日精选新闻',
+        value: 'news.php'
+    } ,{
+        name: '大纪元新闻网',
+        value: '2'
+    }, {
+        name: '功夫寻道',
+        value: 'video/play/1096.html'
     },{
-        name:'大纪元新闻网',
-        value:'2'
+        name: '【共产主义的终极目的】之一：序言',
+        value: 'zKCP'
     },{
-        name:'功夫寻道',
-        value:'video/play/1096.html'
-    },
-    {
-        name:'传奇时代',
-        value:'pVRc'
-    },
-    {
-        name:'过年',
-        value:'wBZT'
-    },
-    {
-        name:'艺考',
-        value:'TnkQ'
-    },
-    {
-        name:'小磊历险记',
-        value:'q183'
-    },
-    {
-        name:'终极目的',
-        value:'npXG'
-    },
-    {
-        name:'街对面的烛光',
-        value:'7CxZ'
-    },
-    {
-        name:'希望之声直播',
-        value:'RjrP'
-    },
-    {
-        name:'铁证如山',
-        value:'video/play/1080.html'
-    },
-    {
-        name:'九评之一',
-        value:'video/play/4.html'
-    },
-    {
-        name:'传奇时代 义诉',
-        value:'video/play/1040.html'
-    },
-    {
-        name:'共产主义的终极目的',
-        value:'video/play/1035.html'
-    },
-    {
-        name:'新唐人中国频道直播',
-        value:'v.php?id=ntd'
-    },
-    {
-        name:'新唐人亚太台直播',
-        value:'v.php?id=ntd-mlt'
-    },
-    {
-        name:'每日精选新闻',
-        value:'news.php'
+        name: '【共产主义的终极目的】之二：中心之国',
+        value: 'yKNT'
+    },{
+        name: '【共产主义的终极目的】之三：马克思魔变',
+        value: '3g2P'
+    },{
+        name: '【共产主义的终极目的】之四：毁灭路线图',
+        value: 'Qd4n'
+    },{
+        name: '【共产主义的终极目的】之五：万变不离其邪',
+        value: '8Xtp'
+    },{
+        name: '【共产主义的终极目的】之六：甜头和苦头',
+        value: 'c5he'
+    },{
+        name: '【共产主义的终极目的】之七：受苦的还是工农',
+        value: 'jD1f'
+    },{
+        name: '【共产主义的终极目的】之八：毁灭人类',
+        value: 'jaKP'
+    },{
+        name: '【共产主义的终极目的】之九：共产邪灵一路杀',
+        value: 'yucm'
+    },{
+        name: '【共产主义的终极目的】之十：苏俄预演 中共登台',
+        value: 'gyR6'
+    },{
+        name: '【共产主义的终极目的】之十一：屠杀精英',
+        value: 'Cavg'
+    },{
+        name: '【共产主义的终极目的】之十二：破坏自然',
+        value: 'DqRH'
+    },{
+        name: '【共产主义的终极目的】之十三：毁灭文化',
+        value: 'hxvC'
+    },{
+        name: '【共产主义的终极目的】之十四：邪恶之最',
+        value: 'Df2G'
+    },{
+        name: '【共产主义的终极目的】之十五：毁人不倦',
+        value: 'qM8z'
+    },{
+        name: '【共产主义的终极目的】之十六：反神骂祖宗',
+        value: 'q7Ba'
+    },{
+        name: '【共产主义的终极目的】之十七：颠倒善恶',
+        value: 'nooV'
+    },{
+        name: '【共产主义的终极目的】之十八：柔性洗脑',
+        value: '7qdh'
+    },{
+        name: '【共产主义的终极目的】之十九：打开地狱门',
+        value: 'DcfF'
+    },{
+        name: '【共产主义的终极目的】之二十：偷天换日',
+        value: 'AoWA'
+    },{
+        name: '【共产主义的终极目的】之廿一 神传语言文字',
+        value: 'U3qQ'
+    },{
+        name: '【共产主义的终极目的】之廿二 破坏修炼文化',
+        value: '2YHh'
+    },{
+        name: '【共产主义的终极目的】之二十三 富而有德',
+        value: 'Wuq9'
+    },{
+        name: '【共产主义的终极目的】之二十四 人伦典范',
+        value: 'MKG3'
+    },{
+        name: '真实的江泽民（一） 经济发展下的道德底线（上）',
+        value: 'Ke1C'
+    },{
+        name: '真实的江泽民（二） 经济发展下的道德底线（下）',
+        value: 'hRFy'
+    },{
+        name: '真实的江泽民（三） 江泽民的GDP（上）',
+        value: 'dQGT'
+    },{
+        name: '真实的江泽民（四） 江泽民的GDP（下）',
+        value: 'PQNu'
+    },{
+        name: '真实的江泽民（五） 道德崩溃后的社会乱像（上）',
+        value: 'XQJ3'
+    },{
+        name: '真实的江泽民（六） 道德崩溃后的社会乱像（下）',
+        value: 'pxvF'
+    },{
+        name: '真实的江泽民（七） 民国的那两年',
+        value: 'JUUQ'
+    },{
+        name: '真实的江泽民（八） 六四镇压的最大受益者',
+        value: 'ChL4'
+    },{
+        name: '真实的江泽民（九） 跨世纪接班人的由来',
+        value: 'S3PT'
+    },{
+        name: '真实的江泽民（十） 江泽民和他的女人们（续）',
+        value: 'gr5S'
+    },{
+        name: '真实的江泽民（十一） 因妒生恶的世纪迫害（上）',
+        value: 'z2h5'
+    },{
+        name: '真实的江泽民（十二） 因妒生恶的世纪迫害（中）',
+        value: 'vkm4'
+    },{
+        name: '真实的江泽民（十三） 因妒生恶的世纪迫害（下）',
+        value: 'u2Nk'
+    },{
+        name: '真实的江泽民（十四） 这个星球前所未有的邪恶',
+        value: '9MpS'
+    },{
+        name: '真实的江泽民（十五） 血债帮崩盘',
+        value: 'kb2A'
+    },{
+        name: '真实的江泽民（十六） 起诉第一罪人',
+        value: 'yHGT'
     }
 ]
